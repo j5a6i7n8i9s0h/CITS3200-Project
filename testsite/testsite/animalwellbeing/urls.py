bfb96604fc1dc940086ebc61130e29b17c1ca65a
Jainish Pithadiya <jainishpithadiya@192-168-1-6.tpgi.com.au>
2018-08-15 00:11:18 +0800

    login/logout basic

--- testsite/testsite/animalwellbeing/urls.py
+++ testsite/testsite/animalwellbeing/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from . import views
-
+app_name = 'awb'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('accounts/login', views.login_view, name='login'),
+    path('accounts/logout',views.logout, name='logout')
 ]
--- testsite/testsite/animalwellbeing/views.py
+++ testsite/testsite/animalwellbeing/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from .forms import *
+from django.shortcuts import render, redirect
+# will improve later down the line
+from .forms import * 
+from .models import * 
 
 #user authentication
 from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
@@ -9,11 +11,25 @@ from django.contrib.auth.models import User, Group
 # Create your views here.
 
 def index(request):
+		return redirect('/awb/accounts/index') if not request.user.is_authenticated else render(request, 'animalwellbeing/index.html',{'user':request.user})
+	
+
+def login_view(request):
 	context = {}
 	if request.method=='POST':
 		form = LoginForm(request.POST)
-		#if form.is_valid(): 
-		username = form['Username']
-		password = form['Password']
+		if form.is_valid(): 
+			username = form.cleaned_data['Username']
+			password = form.cleaned_data['Password']
+			user = authenticate(request, username=username, password=password)
+			if user:
+				login(request, user)
+				return index(request)
+				#success 
+	return render(request, 'animalwellbeing/login.html')
+
+
+
+def profile(request, user):
 
 	return render(request,'animalwellbeing/index.html')
