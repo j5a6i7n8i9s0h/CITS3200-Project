a3f27c2a0effdf2f424c3be1f0a085dc7eebc90c
j5a6i7n8i9s0h <21962504@student.uwa.edu.au>
2018-09-11 00:28:44 +0800

    made editable templates and approval status

--- testsite/testsite/animalwellbeing/views.py
+++ testsite/testsite/animalwellbeing/views.py
@@ -60,6 +60,50 @@ def create_researcher(request):
 			return redirect('/awb/')
 	return render(request, 'animalwellbeing/signup.html')
 
+from .additional_func import standardise_keys
+@login_required
+def edit_form(request, coversheet_id):
+	coversheetmodel = None
+	try:
+		if request.user.is_superuser:
+			coversheetmodel = CoverSheetFormModel.objects.get(pk=coversheet_id)
+		else:
+			coversheetmodel = CoverSheetFormModel.objects.get(pk=coversheet_id, creator=Researchers.objects.get(user=request.user))
+	except CoverSheetFormModel.DoesNotExist:
+		return redirect('/awb/')
+
+	if request.method == 'POST': 
+		form = CoverSheetForm(request.POST)
+		dictionary_data={
+			'contact_details':{
+				'Protocol Title :' : '' or form['protocol_title'].value(),
+				'Monitoring Start Date :':'' or form['start_date'].value(),
+				'Chief Investigator :' :[form['cheif_investigator'].value(), form['cheif_investigator_phone'].value()],
+				'Emergency Contact :': [form['emergency_investigator'].value(), form['emergency_investigator_phone'].value()],
+				'Monitor 1 :': [form['monitor_1'].value(), form['monitor_1_phone'].value()],
+				'Monitor 2 :': [form['monitor_2'].value(), form['monitor_2_phone'].value()],
+				'Monitor 3 :': [form['monitor_3'].value(), form['monitor_3_phone'].value()],
+				'Supervisor :': form['supervision'].value(),
+				'Person responsible for euthanasia :': [form['euthanasia_person'].value(), form['euthanasia_phone'].value()],
+				'Other experts :': [form['other_experts'].value(), form['other_experts_phone'].value()],
+			},
+			'species_phenotype_issues':{
+				'Species' : form['species_phenotype_issues'].value()
+			},
+			'monitoring_criteria':{},
+			'monitoring_frequency':{},
+			'type_of_recording_sheet':{},
+			'actions_and_interventions':{}
+		}
+		coversheetmodel.all_data = dictionary_data
+		coversheetmodel.save()
+		return redirect('/awb/')
+	else:
+		if coversheetmodel.approved:
+			return redirect('/awb/')
+		else:
+			return render(request, 'animalwellbeing/createcoversheet.html',{'dictionary_data':standardise_keys(coversheetmodel.all_data)})
+
 @login_required
 def form_creation(request):
 	if request.method == 'POST':
